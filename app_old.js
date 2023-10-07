ee40ea3eeb90c6a572f0eda72c68eea8c4d6af57
sahabaz <sahabaz517@gmail.com>
2023-10-08 00:09:16 +0530

    ddd

--- app_old.js
+++ app_old.js
@@ -6,7 +6,7 @@ let app = express();
 let movies = JSON.parse(fs.readFileSync('./data/movies.json', 'utf-8'))
 app.use(express.json());
 //route=http method +url
-const getMovie = (req, res) => {
+app.get('/app/v1/movies', (req, res) => {
     res.status(200).json({
         status: 200,
         count: movies.length,
@@ -14,8 +14,8 @@ const getMovie = (req, res) => {
             movies
         }
     })
-}
-const postMovie = (req, res) => {
+})
+app.post('/app/v1/movies', (req, res) => {
     const newId = movies[movies.length - 1].id + 1;
     const movie = { ...{ id: newId }, ...req.body }
     movies = [...movies, movie];
@@ -26,8 +26,8 @@ const postMovie = (req, res) => {
         })
     })
     // console.log(movies)
-}
-const getMovieById = (req, res) => {
+})
+app.get('/app/v1/movies/:id', (req, res) => {
     const movieIdToFind = +req.params.id;
     const foundMovie = movies.find(movies => movies.id === movieIdToFind)
     try {
@@ -47,8 +47,8 @@ const getMovieById = (req, res) => {
     } catch (error) {
         console.error(error.message)
     }
-}
-const updateByPutDetails = (req, res) => {
+})
+app.put('/app/v1/movies/:id', (req, res) => {
     findMovieID = +req.params.id;
     const findMovieIndex = movies.findIndex(movie => movie.id === findMovieID);
     if (findMovieIndex == -1) {
@@ -66,8 +66,8 @@ const updateByPutDetails = (req, res) => {
             data: movie
         })
     })
-}
-const updateByPatchDetail = (req, res) => {
+})
+app.patch('/app/v1/movies/:id', (req, res) => {
     const findId = + req.params.id;
     let getMovieItemById = movies.find(movie => movie.id === findId);
     if (!getMovieItemById) {
@@ -87,8 +87,8 @@ const updateByPatchDetail = (req, res) => {
         })
     })
 
-}
-const DeleteById = (req, res) => {
+})
+app.delete('/app/v1/movies/:id', (req, res) => {
     let deleteMovieId = +req.params.id;
     isValueExist = movies.find(movie => movie.id === deleteMovieId);
     if (!isValueExist) {
@@ -106,15 +106,7 @@ const DeleteById = (req, res) => {
             data: { movies }
         })
     })
-}
-app.route('/app/v1/movies')
-    .get(getMovie)
-    .post(postMovie)
-app.route('/app/v1/movies/:id')
-    .get(getMovieById)
-    .put(updateByPutDetails)
-    .patch(updateByPatchDetail)
-    .delete(DeleteById)
+})
 //create server
 const port = 3000;
 app.listen(port, () => {
