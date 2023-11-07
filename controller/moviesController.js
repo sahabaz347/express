499741392bc3ab9252e758f6c03d1b8e401c814d
sahabaz <sahabaz517@gmail.com>
2023-11-07 22:39:13 +0530

    no

--- controller/moviesController.js
+++ controller/moviesController.js
@@ -122,31 +122,30 @@ class Movie {
             })
         }
     }
-    getMoviesStats=async (req,res)=>{
-        try{
-            let stat=await MovieModel.aggregate([
-                {$match:{rating:{$gte:8}}},
-                {$group:{
-                    _id:'$relaseYear',
-                    avgRating:{$avg:'$rating'},
-                    avgPrice:{$avg:'$price'},
-                    minPrice:{$avg:'$price'},
-                    maxPrice:{$avg:'$price'},
-                    priceTotal:{$sum:'$price'},
-                    movieCount:{$sum:1},
-                }},
-                {$sort:{minPrice:1}}
-
+    getMoviesStats = async (req, res) => {
+        try {
+            let stat = await MovieModel.aggregate([
+                { $match: { rating: { $gte: 8 } } },
+                {
+                    $group: {
+                        _id: '$relaseYear',
+                        avgRating: { $avg: '$rating' },
+                        avgPrice: { $avg: '$price' },
+                        minPrice: { $avg: '$price' },
+                        maxPrice: { $avg: '$price' },
+                        priceTotal: { $sum: '$price' },
+                        movieCount: { $sum: 1 },
+                    }
+                },
+                { $sort: { minPrice: 1 } }
             ])
-            console.log(123,stat)
-
             res.status(200).json({
                 status: 200,
-                length:stat.length,
+                length: stat.length,
                 requestedAt: req.requestedAt,
                 data: stat
             })
-        }catch(error){
+        } catch (error) {
             res.status(400).json({
                 status: 400,
                 requestedAt: req.requestedAt,
